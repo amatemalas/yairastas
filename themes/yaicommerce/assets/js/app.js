99495d15c3d9004010d21bb5f5ea0fe796806b66
Adrián Matemalas <amatemalas@refineriaweb.com>
2023-10-08 20:45:29 +0200

    Services page + animations + faqs block

--- themes/yaicommerce/assets/js/app.js
+++ themes/yaicommerce/assets/js/app.js
@@ -3,6 +3,15 @@ window.Popper = require('popper.js');
 require('bootstrap');
 
 import '@fortawesome/fontawesome-free/js/all.js';
+// import 'aos/dist/aos.js';
 
 require('./modules/navbar');
 require('./modules/slider');
+
+import Aos from 'aos';
+
+$(document).ready(function () {
+    Aos.init({
+        duration: 700,
+    });
+});
